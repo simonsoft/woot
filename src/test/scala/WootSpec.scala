0da441b1ce2c591ab49ec696ca4037d2bf5b8720
Richard Dallaway <richard@dallaway.com>
2013-09-05 15:21:13 +0100

    Unit test for simple insert case.

--- src/test/scala/WootSpec.scala
+++ src/test/scala/WootSpec.scala
@@ -6,7 +6,8 @@ class WootSpec extends Specification {
 
   "Single User Woot" should {
 
-    "support insertion of a WChar" in {
+    
+    "insertion of a WChar at a specific position (internal test)" in {
 
       val w1 = WChar(Id(1,1), 'A') 
       val w2 = WChar(Id(1,2), 'B') 
@@ -19,6 +20,21 @@ class WootSpec extends Specification {
       new WString().ins(w1, 0).ins(w2,1).ins(w3,2).text must_== "ABC"  
     }
 
+    "insertion based on previous and next IDs" in {
+      
+      val w1 = WChar(Id(1,1), 'A') 
+      val w2 = WChar(Id(1,2), 'B') 
+      val w3 = WChar(Id(1,3), 'C') 
+      val s1 = WString().ins(w1,0).ins(w2,1).ins(w3,2)
+      
+      val wx = WChar(Id(1,4), 'X') 
+      s1.insertBetween(wx, s1.neighbours(0)).text must_== "XABC"
+      s1.insertBetween(wx, s1.neighbours(1)).text must_== "AXBC"
+      s1.insertBetween(wx, s1.neighbours(2)).text must_== "ABXC"
+      s1.insertBetween(wx, s1.neighbours(3)).text must_== "ABCX"
+   
+    }
+    
 
   }
 
